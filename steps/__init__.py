22b5226fec57aa9185a9b5951d2fe74ca9761faf
Austin Thapa <anilthapa1.at23@gmail.com>
2025-10-16 22:25:41 -0500

    chore: add __init__.py to define steps directory as package

---
+++ steps/__init__.py
@@ -0,0 +1,5 @@
+from .data_ingestion import ingest_data
+from .data_preprocess import preprocess_data
+from .data_split import split_data
+from .model_train import train_model
+from .model_evaluation import evaluate_model
